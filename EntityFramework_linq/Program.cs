a9181392171efb7995af9e20c640b1b6eac82982
Marcos <marcoscozatti@gmail.com>
2020-10-29 09:52:31 -0300

    Primeira versão

---
+++ EntityFramework_linq/Program.cs
@@ -0,0 +1,27 @@
+﻿using EntityFramework_linq.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_linq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//incluir
+			new RepositorioCategoria().AdicionarCategoria(0, "Carros");
+
+			//Alterar
+			//new RepositorioCategoria().AlterarCategoria(9, "Frutas Doces");
+
+			//Excluir
+			//new RepositorioCategoria().ExcluirCategoria(11);
+
+
+			
+		}
+	}
+}
---
+++ EntityFramework_linq/Repositorio/RepositorioCategoria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework_linq;
+
+namespace EntityFramework_linq.Repositorio
+{
+	class RepositorioCategoria
+	{
+		public void AdicionarCategoria(int id, string nome)
+		{
+			Categoria categoria = new Categoria();
+			categoria.Id = id;
+			categoria.Nome = nome;
+
+			LojaEFEntities ef = new LojaEFEntities();
+			ef.Categoria.Add(categoria);
+
+			ef.SaveChanges();
+
+
+		}
+
+		public void AlterarCategoria(int id, string nome)
+		{
+
+			LojaEFEntities ef = new LojaEFEntities();
+			var categoria = ef.Categoria.First(item => item.Id == id);
+
+			categoria.Nome = nome;
+
+			ef.SaveChanges();
+
+
+		}
+
+		public void ExcluirCategoria(int id)
+		{
+
+			LojaEFEntities ef = new LojaEFEntities();
+			var categoria = ef.Categoria.First(x => x.Id == id);
+			ef.Categoria.Remove(categoria);
+			ef.SaveChanges();
+
+
+		}
+
+
+
+	}
+
+}
